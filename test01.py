0b3760c6f58fb2c14212e809c77e9f4b78096631
runnablek <runnablek@gmail.com>
2014-06-24 21:17:25 +0900

    회사에서

--- test01.py
+++ test01.py
@@ -1,14 +1,37 @@
-import sys
+# -*- coding: UTF-8 -*-
+
+
 
 
 def main ():
+
+	import sys	
 	''' comment...
 	'''
 
 	print(sys.platform + " Hello world!!")
 
 
+def test01():
+
+
+	flag = True
+	if flag :
+		print ("flag is {}".format(flag))
+
+
+
+def test02():
+
+	import os	
+	filename = os.environ.get('PYTHONSTARTUP')
+	print("file name is {}".format(filename))
+
+
+
 
 if __name__ == '__main__' :
 	main()
+	test01()
+	test02()
 
