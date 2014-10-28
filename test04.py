5bf8e62bcb8643b058f49d95bff679d7d2ea8ba1
Administrator <runkyo@ubuntu.(none)>
2014-10-28 23:28:13 +0900

    ddd

--- test04.py
+++ test04.py
@@ -1,1 +1,15 @@
 
+class Student:
+		name = "choi"
+		def info(self):
+			print("my name is " + self.name +"!!")
+
+
+
+inst = Student()
+
+print(type(inst))
+#inst.info()
+
+print("aaaa")
+print("bbbbs")
